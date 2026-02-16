6d079061ebca7449c2d60ebad0f8aa1a1edc4718
dhanyaramkumar1890-ai <dhanyaramkumar1890@gmail.com>
2026-02-16 11:14:14 +0530

    Add files via upload

--- firebase-config.template.js
+++ firebase-config.template.js
@@ -11,24 +11,4 @@
 //
 // IMPORTANT: Never commit this file with real credentials to public repositories!
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAwOEeOVG29O8DJRzmEOKxmRXoR8R2b5cU",
-    authDomain: "omnixam---dl.firebaseapp.com",
-    projectId: "omnixam---dl",
-    storageBucket: "omnixam---dl.firebasestorage.app",
-    messagingSenderId: "218730322717",
-    appId: "1:218730322717:web:f7e90b2c1a232538435ff4",
-    measurementId: "G-9X4XCBZKV6"
-  };
 
-// Example of a real config (these are fake values):
-// const firebaseConfig = {
-//     apiKey: "AIzaSyB1234567890abcdefghijklmnopqrst",
-//     authDomain: "examhub-india.firebaseapp.com",
-//     projectId: "examhub-india",
-//     storageBucket: "examhub-india.appspot.com",
-//     messagingSenderId: "123456789012",
-//     appId: "1:123456789012:web:abc123def456"
-// };
-
-export default firebaseConfig;
